7439c0ae28be99fb348afba67cb85f658d3f1400
Michael Lehmann <lehmamic@bluewin.ch>
2020-05-12 22:40:44 +0200

    fix tslint issues

--- src/Demo.Frontend/src/app/demo/demo/demo.component.ts
+++ src/Demo.Frontend/src/app/demo/demo/demo.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
-  styleUrls: ['./demo.component.scss']
+  styleUrls: ['./demo.component.scss'],
 })
 export class DemoComponent implements OnInit {
 
